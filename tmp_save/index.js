c963dfd60b7383c1abdabc2940ae0e6d1cb5e908
Acan <xie.zeyu20@gmail.com>
2024-02-01 16:18:33 +0800

    .

---
+++ tmp_save/index.js
@@ -0,0 +1,29 @@
+const _files = document.getElementById("files");
+
+_files.onchange = () => {
+
+    // console.log(_files.files.length);
+
+    Array.from(_files.files).forEach(file => {
+
+        let formData = new FormData();
+
+        formData.append('file', file);
+
+        console.log(formData)
+
+        fetch('http://118.195.252.2:3000/upload', {
+            method: 'POST',
+            body: formData
+        }).then(response => {
+            if (response.ok) {
+                console.log('文件上传成功');
+            } else {
+                console.error('文件上传失败');
+            }
+        }).catch(error => {
+            console.error('发生错误：', error);
+        });
+
+    });
+}
