70a3ea7c4da4b945e07bc536f98cd5a43e5578cf
Acan <xie.zeyu20@gmail.com>
2024-02-01 14:32:44 +0800

    .

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const multer = require("multer")
+const config = require("./config.json")
+
+// Express Server
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./data");
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        cb(null, `${file.fieldname}-${Date.now()}${ext}`);
+    },
+});
+const upload = multer({ storage: storage });
+
+app.post("/upload", upload.single("photo"),(req, res) => {
+
+    const photo = req.file;
+
+    if (!photo) {
+        return res.status(400).send("File Not Received");
+    }
+
+    res.send("Success");
+})
+
+// Listen
+app.listen(config.port, () => {
+    console.log("Server Created")
+})
