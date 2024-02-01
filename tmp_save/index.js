fc04049ba5ec69f58c47a24d0d2c704b83684836
Acan <xie.zeyu20@gmail.com>
2024-02-01 18:55:31 +0800

    Update index.js

--- tmp_save/index.js
+++ tmp_save/index.js
@@ -2,15 +2,11 @@ const _files = document.getElementById("files");
 
 _files.onchange = () => {
 
-    // console.log(_files.files.length);
-
     Array.from(_files.files).forEach(file => {
 
         let formData = new FormData();
 
         formData.append('file', file);
-
-        console.log(formData)
 
         fetch('http://118.195.252.2:3000/upload', {
             method: 'POST',
