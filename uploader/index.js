363e1641d4f28164538c1add82d0e0914750f7b4
Acan <xie.zeyu20@gmail.com>
2024-02-02 20:40:21 +0800

    .

--- uploader/index.js
+++ uploader/index.js
@@ -32,8 +32,9 @@ fetch("http://118.195.252.2:3000/log").then(res => res.json()).then(res => {
 // Copy Log
 _part_of_log.onclick = () => {
     const date = _date.value;
-    if(date) {
-        copyToClipboard(log_json[date+".txt"])
+    if (date) {
+        copyToClipboard(log_json[date + ".txt"])
+        window.alert("Part of Log Copied to Clipboard")
     }
     else {
         window.alert("No Date Selected");
@@ -41,27 +42,48 @@ _part_of_log.onclick = () => {
 }
 _full_log.onclick = () => {
     copyToClipboard(log_text);
+    window.alert("Full Log Copied to Clipboard")
 }
 
 // Submit Form
-_form.onsubmit = () => {
+_form.onsubmit = async () => {
+    let total_num = Array.from(_files.files).length;
+    let success_num = 0;
+
+    let fetchPromises = [];
+
     Array.from(_files.files).forEach(file => {
         let formData = new FormData();
-        formData.append('file', file);
-        fetch('http://118.195.252.2:3000/upload', {
-            method: 'POST',
-            body: formData
-        }).then(response => {
-            if (response.ok) {
-                console.log('Successful');
-            } else {
-                console.error('Failed');
-            }
-        }).catch(error => {
-            console.error('ERROR: ' + error);
-        });
+        formData.append("file", file);
+
+        fetchPromises.push(
+            fetch('http://118.195.252.2:3000/upload', {
+                method: 'POST',
+                body: formData
+            }).then(response => {
+                if (response.ok) {
+                    console.log("File Upload Successful");
+                    success_num += 1;
+                } else {
+                    console.error("File Upload Failed");
+                }
+            }).catch(error => {
+                console.error(error);
+            })
+        );
     });
-}
+
+    Promise.all(fetchPromises).then(() => {
+        try {
+            window.alert(`Process finished. Total ${total_num}, success${success_num}. `)
+        } catch {
+            console.error(error);
+        }
+    }).catch(err => {
+        console.error(err)
+    })
+
+};
 
 // Reset Form
 _reset.onclick = () => {
